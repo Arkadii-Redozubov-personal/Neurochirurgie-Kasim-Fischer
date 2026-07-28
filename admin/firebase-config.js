5c78672d3269eb6a50c2ff6b07bb950233b5e031
arkady300iq <arkadijrivior@gmail.com>
2026-07-28 14:33:17 +0300

    changes + admin panel

---
+++ admin/firebase-config.js
@@ -0,0 +1,12 @@
+// Firebase Configuration for vertex-clinic
+// WARNING: Keep this file secure and do not share it publicly
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBNcdljYigWs6nlFKy5VAVo3VPicHRQn9M",
+  authDomain: "vertex-clinic.firebaseapp.com",
+  projectId: "vertex-clinic",
+  storageBucket: "vertex-clinic.firebasestorage.app",
+  messagingSenderId: "385329227649",
+  appId: "1:385329227649:web:5f1b4244ac3990b4a26a80",
+  measurementId: "G-2C1LDNTRLL"
+};
